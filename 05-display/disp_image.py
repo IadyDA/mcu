2cbdd22cec1c3f5cc14da37a1a65cec24531df49
LadyDA <khabibullina.dm@phystech.edu>
2026-03-26 14:52:54 +0300

    Исправили картинку

--- 05-display/disp_image.py
+++ 05-display/disp_image.py
@@ -1,16 +1,14 @@
 from PIL import Image
 import time
 import serial
-import matplotlib.pyplot as plt
 
 def main():
-    ser = serial.Serial(port='COM3', baudrate=115200, timeout=0.0)
+    ser = serial.Serial(port='COM5', baudrate=115200, timeout=1)
 
-    image = Image.open('5 Как работать с дисплеем/Задания/pics/get.jpg')
+    image = Image.open('pigs.png').convert('RGB')
     width, height = image.size
 
     ser.write("disp_screen 0x000000\n".encode('ascii'))
-
     time.sleep(0.1)
 
     for y in range(height):
@@ -20,10 +18,36 @@ def main():
 
             color = (r << 16) | (g << 8) | b
             
-            ser.write("disp_px {x} {y} {color:06X}\n".encode('ascii'))
+            ser.write(f"disp_px {x} {y} {color:06X}\n".encode('ascii'))
+            
 
     time.sleep(0.1)
     ser.close()
 
 if __name__ == "__main__":
     main()
+
+# import time
+# import serial
+# from PIL import Image
+
+# def main():
+#     ser = serial.Serial(port='COM5', baudrate=115200, timeout=1)
+    
+#     image = Image.open('pigs.png').convert('RGB')
+#     width, height = image.size
+    
+#     ser.write("disp_screen 000000\n".encode('ascii'))
+#     time.sleep(0.1)
+    
+#     for y in range(height):
+#         for x in range(width):
+#             r, g, b = image.getpixel((x, y))
+#             color = (r << 16) | (g << 8) | b
+#             ser.write(f"disp_px {x} {y} {color:06X}\n".encode('ascii'))
+    
+#     time.sleep(0.1)
+#     ser.close()
+
+# if __name__ == "__main__":
+#     main()
